8c3181496e5c2413a36fce23175599edd25aac81
idfarbanecha <faridchaabane23@gmail.com>
2020-01-17 16:20:58 +0100

    now keeping user queries in tables

--- rules/combine_tables.py
+++ rules/combine_tables.py
@@ -3,7 +3,12 @@ Main
 '''
 
 import pandas as pd
-df_list=[pd.read_csv(i,delimiter='\t') for i in snakemake.input]
+df_list=[]
+for file in snakemake.input:
+    entry=file.split('/')[2].split('.tsv')[0]
+    tb=pd.read_csv(file,sep='\t')
+    tb.insert(loc=0, column='UserInputNames', value=[entry]*len(tb))
+    df_list.append(tb)
 df=pd.concat(df_list,sort=False)
 df=df.replace({'Refseq_category':{0:'reference genome',1:'representative genome',6:'na'},'AssemblyStatus':{2:'Complete Genome',3:
                                      'Chromosome',4:'Scaffold',5:'Contig',6:'na'}})
