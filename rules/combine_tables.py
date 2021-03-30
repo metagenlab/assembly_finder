b29bcd96b1fbd256b3060c6c694e901d78a2f2c4
idfarbanecha <faridchaabane23@gmail.com>
2021-03-30 17:18:30 +0200

    added f strings

--- rules/combine_tables.py
+++ rules/combine_tables.py
@@ -1,21 +1,17 @@
-'''
-Main
-'''
-
 import pandas as pd
-df_list=[]
+"""
+Main
+"""
+df_list = []
 for file in snakemake.input:
-    entry=file.split('/')[2].split('.tsv')[0]
-    tb=pd.read_csv(file,sep='\t')
+    entry = file.split('/')[2].split('.tsv')[0]
+    tb = pd.read_csv(file, sep='\t')
     tb.insert(loc=0, column='UserInputNames', value=[entry]*len(tb))
     df_list.append(tb)
-df=pd.concat(df_list,sort=False)
-df=df.replace({'Refseq_category':{0:'reference genome',1:'representative genome',6:'na'},'AssemblyStatus':{2:'Complete Genome',3:
-                                     'Chromosome',4:'Scaffold',5:'Contig',6:'na'}})
-links=list(df['FtpPath_Genbank'])
-filenames=[]
-for i in links:
-    splt=i.split('/')
-    filenames.append(splt[len(splt)-1])
-df.insert(loc=0,column='AssemblyNames',value=filenames)
-df.to_csv(snakemake.output[0],sep='\t',index=False)
+df = pd.concat(df_list,sort=False)
+df = df.replace({'Refseq_category': {0: 'reference genome', 1: 'representative genome', 6: 'na'},
+                 'AssemblyStatus': {2: 'Complete Genome', 3: 'Chromosome', 4: 'Scaffold', 5: 'Contig', 6: 'na'}})
+links = list(df['FtpPath_Genbank'])
+filenames = [link.split('/')[-1].replace('-', '_') for link in links]
+df.insert(loc=0, column='AssemblyNames', value=filenames)
+df.to_csv(snakemake.output[0], sep='\t', index=None)
--- rules/dl.py
+++ rules/dl.py
@@ -1,28 +1,27 @@
 import logging
-
-logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',datefmt='%d %b %Y %H:%M:%S',filename=snakemake.log[0], level=logging.DEBUG)
-
 import os
 import ftplib
 import pandas as pd
+logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', datefmt='%d %b %Y %H:%M:%S',
+                    filename=snakemake.log[0], level=logging.DEBUG)
 
 
-def dl_fna_ftp(ftp_login,input_table):
-        table=pd.read_csv(input_table,sep='\t',index_col=0)
-        full_path=table.loc[snakemake.wildcards.assemblyname]['FtpPath_Genbank']
-        split_path=full_path.split('/')
-        redundant_name=split_path[-1]
-        path=full_path.split('ftp://ftp.ncbi.nlm.nih.gov/')[1]
+def dl_fna_ftp(ftp_login, input_table):
+        table = pd.read_csv(input_table,sep='\t',index_col=0)
+        full_path = table.loc[snakemake.wildcards.assemblyname]['FtpPath_Genbank']
+        split_path = full_path.split('/')
+        redundant_name = split_path[-1]
+        path = full_path.split('ftp://ftp.ncbi.nlm.nih.gov/')[1]
         ftp_login.cwd(path)
-        logging.info('Downloading from : {0}'.format(path))
-        logging.info('Downloading to {0}'.format(snakemake.output[0]))
-        ftp_login.retrbinary("RETR "+redundant_name+'_genomic.fna.gz',
-                             open(os.path.join(os.getcwd(),snakemake.output[0]),"wb").write)
+        logging.info(f'Downloading from : {path}')
+        logging.info(f'Downloading to {snakemake.output[0]}')
+        ftp_login.retrbinary("RETR " + redundant_name + '_genomic.fna.gz',
+                             open(os.path.join(os.getcwd(), snakemake.output[0]), "wb").write)
         logging.info('Done')
 
-'''
-Main
-'''
-ftp=ftplib.FTP(host='ftp.ncbi.nih.gov',user='anonymous',passwd=snakemake.params['NCBI_email'])
 
-dl_fna_ftp(ftp,snakemake.input[0])
+"""
+Main
+"""
+ftp = ftplib.FTP(host='ftp.ncbi.nih.gov', user='anonymous', passwd=snakemake.params['NCBI_email'])
+dl_fna_ftp(ftp, snakemake.input[0])
