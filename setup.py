94f4077f9f6e51ffd99f68d57bbcf728c783a12f
Trestan Pillonel <trestan.pillonel@gmail.com>
2021-07-20 15:51:46 +0200

    added setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+import glob
+import os
+import pkg_resources
+from assembly_finder import __version__
+
+setup(name='assembly_finder',
+      version=__version__,
+      packages=find_packages(),
+      package_data={"":["assembly_finder/*", ]},
+      description='Download assemblies from NCBI',
+      url='https://github.com/metagenlab/assembly_finder',
+      author='Farid Chaabane & Trestan Pillonel',
+      author_email='trestan.pillonel@gmail.com',
+      entry_points="""
+      [console_scripts]
+     af = assembly_finder.af:cli
+      """,
+      include_package_data=True,
+      keywords=[],
+      zip_safe=False)
