5b5f7b73de8f6600adae64d1129ef017912e65ab
farchaab <farid.chaabane@chuv.ch>
2023-09-15 16:07:28 +0200

    cleaned format

--- setup.py
+++ setup.py
@@ -1,21 +1,18 @@
 from setuptools import setup, find_packages
-import glob
-import os
-import pkg_resources
 from assembly_finder import __version__
 
 setup(name='assembly_finder',
       version=__version__,
-      packages=find_packages(),
-      package_data={"":["assembly_finder/*", ]},
       description='Snakemake pipeline for downloading assemblies from NCBI',
       url='https://github.com/metagenlab/assembly_finder',
       author='Farid Chaabane & Trestan Pillonel',
       author_email='trestan.pillonel@gmail.com',
-      entry_points="""
-      [console_scripts]
-     assembly_finder = assembly_finder.assembly_finder:cli
-      """,
+      packages=find_packages(),
       include_package_data=True,
-      keywords=[],
-      zip_safe=False)
+      data_files=[(".", ["LICENSE", "README.md"])],
+      entry_points = {
+          'console_scripts' : [
+              'assembly_finder = assembly_finder.assembly_finder:cli'
+              ]
+      }
+)
