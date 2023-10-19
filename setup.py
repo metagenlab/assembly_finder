2ac37e012e542c19a5a3c69a49aa7c2b5f0ba26b
farchaab <farid.chaabane@chuv.ch>
2023-10-19 13:56:38 +0200

    add package data

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 from setuptools import setup, find_packages
 from assembly_finder import __version__
 
-setup(name='assembly_finder',
-      version=__version__,
-      description='Snakemake pipeline for downloading assemblies from NCBI',
-      url='https://github.com/metagenlab/assembly_finder',
-      author='Farid Chaabane & Trestan Pillonel',
-      author_email='trestan.pillonel@gmail.com',
-      packages=find_packages(),
-      include_package_data=True,
-      data_files=[(".", ["LICENSE", "README.md"])],
-      entry_points = {
-          'console_scripts' : [
-              'assembly_finder = assembly_finder.assembly_finder:cli'
-              ]
-      }
+setup(
+    name="assembly_finder",
+    version=__version__,
+    description="Snakemake pipeline for downloading assemblies from NCBI",
+    url="https://github.com/metagenlab/assembly_finder",
+    author="Farid Chaabane & Trestan Pillonel",
+    author_email="trestan.pillonel@gmail.com",
+    packages=find_packages(),
+    package_data={"assembly_finder": ["Snakefile", "bin/*"]},
+    data_files=[(".", ["LICENSE", "README.md"])],
+    entry_points={
+        "console_scripts": ["assembly_finder = assembly_finder.assembly_finder:cli"]
+    },
 )
