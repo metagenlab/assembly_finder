188fcbd04985ba82e9f4f978c18388fc0ad73ae9
farchaab <farid.chaabane@chuv.ch>
2024-03-21 17:03:35 +0100

    added install requirements

--- setup.py
+++ setup.py
@@ -4,12 +4,20 @@ from assembly_finder import __version__
 setup(
     name="assembly_finder",
     version=__version__,
-    description="Snakemake pipeline for downloading assemblies from NCBI",
+    description="Snakemake cli for downloading genomes with NCBI datasets",
     url="https://github.com/metagenlab/assembly_finder",
-    author="Farid Chaabane & Trestan Pillonel",
-    author_email="trestan.pillonel@gmail.com",
+    python_requires=">=3.9",
+    author="Farid Chaabane, Trestan Pillonel",
+    author_email="farid.chaabane@chuv.ch, trestan.pillonel@gmail.com",
     packages=find_packages(),
     package_data={"assembly_finder": ["Snakefile", "bin/*"]},
+    py_modules=["assembly_finder"],
+    install_requires=[
+        "snakemake>=7.32.4,<8",
+        "pandas>=2.0.2",
+        "pulp<2.8",
+        "rich-click>=1.7.4",
+    ],
     data_files=[(".", ["LICENSE", "README.md"])],
     entry_points={
         "console_scripts": ["assembly_finder = assembly_finder.__main__:cli"]
